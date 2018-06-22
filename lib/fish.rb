dccf20245ea0c4438ed4d35f75ec5fcbb87e90b7
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-22 16:13:46 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,9 @@
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name,:owner
+
+  def initialize(name)
+    @name = name.freeze
+    @mood = "nervous"
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,95 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor :name, :pets,:fishes,:dogs,:cats,:owner
+  attr_reader :species
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "human".freeze
+    @pets = {:fishes => [],:dogs => [],:cats => []}
+    save
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.each do |arrays|
+      arrays.each do |pets|
+        pets.each do |pet|
+          pet.mood = "nervous"
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+  def buy_fish(fish)
+    fish = Fish.new(fish)
+    pets[:fishes] << fish
+    save
+  end
+
+  def buy_dog(dog)
+    dog = Dog.new(dog)
+    pets[:dogs] << dog
+    save
+  end
+
+  def buy_cat(cat)
+    cat = Cat.new(cat)
+    pets[:cats] << cat
+    save
+  end
+
+  def say_species
+  "I am a #{@species}."
+  end
+
+  def species
+    @species
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+  def self.count
+    @@all.size
+  end
+
 end
